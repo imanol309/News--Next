10709798f1cea2b951f9f93ad2f571ae785f0817
imanol309 <imanolguzman309@gmail.com>
2023-02-15 16:01:48 -0400

    Agregando diferentes configuraciones

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Home News</title>
         <meta name="description" content="Home News" />
         <link rel="icon" href="/newsIcon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main>
         <div>Hola mundo</div>
       </main>
     </div>
