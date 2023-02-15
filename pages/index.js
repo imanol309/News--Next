ef182d0dddc04f728c86225a2a5b26b5cfd88954
imanol309 <imanolguzman309@gmail.com>
2023-02-15 16:46:06 -0400

    Cambios

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gradient-to-r from-slate-900 via-purple-900 to-slate-900">
       <Head>
         <title>Home News</title>
         <meta name="description" content="Home News" />
